bcbcf6fc53fb3e01d52cac81c31dec1666b871bf
juliendu11 <ares11@outlook.fr>
2025-10-20 14:37:55 +0200

    Add umami

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_UMAMI_SOURCE?: string
+  readonly VITE_UMAMI_KEY?: string
+}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// REMOVE: this is only for https://verto.julien-dacosta.dev/
+if (import.meta.env.VITE_UMAMI_SOURCE && import.meta.env.VITE_UMAMI_KEY) {
+  const s = document.createElement('script')
+  s.defer = true
+  s.src = import.meta.env.VITE_UMAMI_SOURCE
+  s.setAttribute('data-website-id', import.meta.env.VITE_UMAMI_KEY)
+  document.head.appendChild(s)
+}
+
 const app = createApp(App)
 
 app.mount('#app')
