f0943d87e501c8013b1883334ee083ca2f07e937
juliendu11 <ares11@outlook.fr>
2025-10-21 12:46:20 +0200

    Add about dialog

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
   <main class="max-w-5xl mx-auto p-6 space-y-6">
-    <header class="flex items-end justify-between">
+    <header class="flex items-center justify-between">
       <div>
         <h1 class="text-2xl font-semibold">{{ t('title') }}</h1>
         <p class="text-sm opacity-60 mt-2">
           {{ t('description') }}
         </p>
+      </div>
+      <div>
+        <button class="btn btn-link" onclick="aboutModal.showModal()">
+          {{ t('navs.about') }}
+        </button>
+        <dialog id="aboutModal" class="modal">
+          <div class="modal-box">
+            <h3 class="text-lg font-bold mb-5">{{ t('dialogs.about.title') }}</h3>
+            <p class="mb-5">
+              <strong>{{ t('dialogs.about.details.developer.label') }} </strong>
+              Julien Da costa
+            </p>
+            <small class="opacity-80">
+              {{ t('dialogs.about.text') }}
+              <a href="https://julien-dacosta.dev" class="underline">
+                https://julien-dacosta.dev
+              </a>
+            </small>
+            <div class="modal-action">
+              <form method="dialog">
+                <button class="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
       </div>
     </header>
 
